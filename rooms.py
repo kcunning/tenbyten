03de21f3859abed722c89dcb91e3b237f9fa519f
Katie Cunningham <katie.fulton@gmail.com>
2010-07-01 12:02:28 -0400

    Rooms have directions, etc

--- rooms.py
+++ rooms.py
@@ -1,27 +1,49 @@
+
 """
 Rooms have the following structure:
 
 Title
 Description
-Contents
 Exits (dictionary)
 """
 
 
-def create_room():
-    pass
+class Room:
     
-def delete_room():
-    pass
+    DIRECTIONS = {
+        'north' :   'south',
+        'south' :   'north',
+        'east'  :   'west',
+        'west'  :   'east',
+        'up'    :   'down',
+        'down'  :   'up',
+    }
     
-def show_room():
-    pass
+    def __init__(self, title='', description='', exits={}):
+        self.title = title
+        self.description = description
+        self.exits = exits
+        for d, r in exits.items():
+            self.connect_rooms(d, r)
     
-def update_room():
-    pass
+    def display(self):
+        print self.title
+        print self.description
+        if self.exits.keys():
+            print "Exits: "
+            for i in self.exits.keys():
+                print i
+        else:
+            print "No exits"
     
-def list_rooms():
-    pass
+    def update_room(self, title='', description='', exits={}):
+        if title:
+            self.title = title
+        if description:
+            self.description = description
+        if exits:
+            for d, r in exits.items():
+                self.connect_rooms(d,r)
     
-def connect_rooms():
-    pass
+    def connect_rooms(self, direction, room):
+        room.exits[self.DIRECTIONS[direction]] = self
