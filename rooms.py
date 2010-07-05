1622417972f018e7c0d8f5d27ca9a0a961d31f37
Katie Cunningham <katie.fulton@gmail.com>
2010-07-05 12:26:11 -0400

    Can now create dungeons and new rooms!

--- rooms.py
+++ rooms.py
@@ -21,7 +21,7 @@ DIRECTIONS = {
 
 class Room:
     
-    def __init__(self, title='', description=''):
+    def __init__(self, title='', description='', exits={}):
         self.title = title
         self.description = description
         self.exits = {}
@@ -41,15 +41,83 @@ class Room:
         else:
             print "\tNo exits"
             
+class Dungeon:
+    
+    def __init__(self, title='', description='', rooms = []):
+        self.title = title
+        self.description = description
+        self.rooms = rooms
+        
+    def display(self):
+        print self.title
+        print '\t', self.description
+        print "Rooms: "
+        self.list_rooms()
+    
+    def list_rooms(self):
+        for r in self.rooms:
+            print r.title
             
-def connect_rooms(r1, dir, r2):
-    """
-    The lexicon would be 'room two is [direction] of room one.
-    """
-    print 'I am connecting rooms ', r1.title, 'and ', r2.title
-    print r1.title, ' has exits ', r1.exits 
-    print r2.title, ' has exits ', r2.exits
-    dir = DIRECTIONS[dir]
-    print dir
-    r2.exits[dir] = r1
-    print r2.exits
+    def menu(self):
+        while 1==1:
+            print "What would you like to do?"
+            print "Edit [d]ungeon | Edit a [r]oom | D[i]splay dungeon | [q]uit"
+            c = raw_input()[0]
+            if c.lower() == 'd':
+                self.update()
+            if c.lower() == 'r':
+                self.rooms_menu()
+            if c.lower() == 'i':
+                self.display()
+            if c.lower() == 'q':
+                break
+    
+    def update(self):
+        title = raw_input("New title (blank for no change): ")
+        description = raw_input("New description (blank for no change): ")
+        if title:
+            self.title = title
+        if description:
+            self.description = description
+        
+    def rooms_menu(self):
+        while 1==1:
+            c = raw_input("Create a [n]ew room | [E]dit a room | [D]elete a room | Go [b]ack ")[0]
+            if c.lower() == 'n':
+                self.new_room()
+            if c.lower() == 'e':
+                self.edit_room()
+            if c.lower() == 'd':
+                pass
+            if c.lower() == 'b':
+                break
+                
+    def new_room(self):
+        """
+        Adds a new room, and connects it to other rooms if needed
+        """
+        title = raw_input("Title: ")
+        description = raw_input("Description: ")
+        while 1==1:
+            c = raw_input("Do you want to add an exit? [y/n] ")[0]
+            exits = {}
+            if c.lower() == 'y':
+                while c.lower() == 'y':
+                    dir = raw_input("Direction: [north/south/east/west/up/down] ")
+                    print "Which room?"
+                    for r in self.rooms:
+                        print self.rooms.index(r), r.title 
+                    e = raw_input("Room #: ")
+                    exits[dir] = self.rooms[int(e)]
+                    c = raw_input("Would you like to add another exit? [y/n] ")
+                break 
+            if c.lower() == 'n':
+                break
+        new_room = Room(title=title, description=description)
+        self.rooms.append(new_room)
+        for i in exits:
+            self.rooms[self.rooms.index(new_room)].set_exit(i,exits[i])
+            
+        
+            
+
