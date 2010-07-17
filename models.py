0f782fd7b1948816a493bd7a89ffeea06e4a11f7
Katie Cunningham <katie.fulton@gmail.com>
2010-07-17 10:14:03 -0400

    Doing more MVC clean-up

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 
 DIRECTIONS = {
@@ -9,18 +10,32 @@ DIRECTIONS = {
     'down'  :   'up',
 }
 
-DIR_CHOICES = ( 'north',
-                'south',
-                'east',
-                'west',
-                'up',
-                'down',
-                'n',
-                'e',
-                'w',
-                's',
-                'u',
-                'd')
+DIR_CHOICES = { 'north': 'north',
+                'south': 'south',
+                'east': 'east',
+                'west': 'west',
+                'up':'up',
+                'down':'down',
+                'n': 'north',
+                'e': 'east',
+                'w': 'west',
+                's': 'south',
+                'u': 'up',
+                'd': 'down'}
+                
+ACTIONS = {'look': 'look',
+           'take': 'take',
+           'go': 'go',
+           'i': 'inventory',
+           'l': 'look',
+           'inventory': 'inventory'}
+           
+def write(string):
+    sys.stdout.write(string+'\n')
+    
+def input(prompt="> "):
+    return raw_input(prompt)
+
 
 class Room:
     title = ""
@@ -37,15 +52,15 @@ class Room:
         self.description = description
    
     def display_room(self, admin=False):
-        print self.title
-        print '\t', self.description
-        print "\tExits: "
+        write(self.title)
+        write('\t' + self.description)
+        write("\tExits:")
         exits = self.get_exits()
         if exits:
             for i in exits:
-                print "\t\t", i
+                write("\t\t" + i)
         else:
-                print "\t\tNo exits."
+                write("\t\tNo exits.")
 
     def get_exits(self):
         exits = []
@@ -55,8 +70,8 @@ class Room:
         return exits
             
     def edit(self):
-        self.title = raw_input("New title (blank for no change): ")
-        self.description = raw_input("New description (blank for no change): ")
+        self.title = input("New title (blank for no change): ")
+        self.description = input("New description (blank for no change): ")
     
     def do_trigger(self):
         pass
@@ -68,7 +83,6 @@ class Room:
                       'west': self.west}
     
     def set_exit(self, direction, room2):
-        print 'set_exit called with', self.title, direction, room2.title
         if direction == 'north':
             self.north=room2
             room2.south=self
@@ -96,20 +110,20 @@ class Dungeon:
         self.rooms = rooms
         
     def display(self):
-        print self.title
-        print '\t', self.description
-        print "Rooms: "
+        write(self.title)
+        write('\t' + self.description)
+        write("Rooms: ")
         self.list_rooms()
     
     def list_rooms(self):
         for r in self.rooms:
-            print self.rooms.index(r), r.title
+            write(self.rooms.index(r) + r.title)
             
     def admin_menu(self):
         while 1==1:
-            print "What would you like to do?"
-            print "Edit [d]ungeon | Edit a [r]oom | D[i]splay dungeon | Add a [c]onnection | [q]uit"
-            c = raw_input()[0]
+            write("What would you like to do?")
+            write("Edit [d]ungeon | Edit a [r]oom | D[i]splay dungeon | Add a [c]onnection | [q]uit")
+            c = input()[0]
             if c.lower() == 'd':
                 self.update()
             if c.lower() == 'r':
@@ -122,8 +136,8 @@ class Dungeon:
                 break
             
     def update(self):
-        title = raw_input("New title (blank for no change): ")
-        description = raw_input("New description (blank for no change): ")
+        title = input("New title (blank for no change): ")
+        description = input("New description (blank for no change): ")
         if title:
             self.title = title
         if description:
@@ -131,7 +145,7 @@ class Dungeon:
         
     def rooms_menu(self):
         while 1==1:
-            c = raw_input("Create a [n]ew room | [E]dit a room | [D]elete a room | Go [b]ack ")[0]
+            c = input("Create a [n]ew room | [E]dit a room | [D]elete a room | Go [b]ack ")[0]
             if c.lower() == 'n':
                 self.new_room()
             if c.lower() == 'e':
@@ -140,30 +154,25 @@ class Dungeon:
                 pass
             if c.lower() == 'b':
                 break
-                
-    def connect_rooms(self):
-        pass
             
-            
-        
     def new_room(self):
         """
         Adds a new room, and connects it to other rooms if needed
         """
-        title = raw_input("Title: ")
-        description = raw_input("Description: ")
+        title = input("Title: ")
+        description = input("Description: ")
         while 1==1:
-            c = raw_input("Do you want to add an exit? [y/n] ")[0]
+            c = input("Do you want to add an exit? [y/n] ")[0]
             exits = {}
             if c.lower() == 'y':
                 while c.lower() == 'y':
-                    dir = raw_input("Direction: [north/south/east/west/up/down] ")
-                    print "Which room?"
+                    dir = input("Direction: [north/south/east/west/up/down] ")
+                    write("Which room?")
                     for r in self.rooms:
-                        print self.rooms.index(r), r.title 
-                    e = raw_input("Room #: ")
+                        write(self.rooms.index(r) + r.title)
+                    e = input("Room #: ")
                     exits[dir] = self.rooms[int(e)]
-                    c = raw_input("Would you like to add another exit? [y/n] ")
+                    c = input("Would you like to add another exit? [y/n] ")
                 break 
             if c.lower() == 'n':
                 break
@@ -172,13 +181,13 @@ class Dungeon:
             
     def edit_room(self):
         self.list_rooms()
-        c = raw_input("Which room? [#] ")
+        c = input("Which room? [#] ")
         
 class Mob:
-    location = Room()
-    dungeon = Dungeon()
-    title = ""
-    description = ""
+    location = None
+    dungeon = None
+    title = None
+    description = None
     
     def __init__(self, location=Room(), dungeon = Dungeon(), title="", description=""):
         self.location=location
@@ -188,27 +197,81 @@ class Mob:
         
     
 class User(Mob):
+    inventory = []
     
-    def __init__(self, location=Room(), dungeon=Dungeon(), title="", description=""):
+    def __init__(self, location=Room(), dungeon=Dungeon(), title="", description="", inventory=[]):
         Mob.__init__(self, location=location, title=title, description=description, dungeon=dungeon)
+        self.inventory = inventory
     
     def move(self, direction):
-        self.location = self.location.exits[direction]
+        exits = self.location.get_exits()
+        if direction in exits:
+            self.location = self.location.exits[direction]
+        else:
+            write("That's not a valid exit.")
         self.location.display_room()
         
+    def action(self, action):
+        if action[0].lower() == 'go':
+            action.pop(action.index('go'))
+            dir = None
+            for i in action:
+                if i in DIR_CHOICES.keys():
+                    dir = i
+            if dir:
+                self.move(DIR_CHOICES[dir.lower()])
+            else:
+                write("You need to give me a direction if you want to go somewhere.")
+                self.location.display_room()
+
+    def add_to_inventory(self, item):
+        self.inventory.append(item)
+        write(item.title + "added to inventory.")
+            
+    
+    def parse_action(self, action):
+        action = action.split()
+        if action[0] in ACTIONS.keys():
+            self.action(action)
+            return
+        if action[0].lower() in DIR_CHOICES.keys():
+            self.move(DIR_CHOICES[action[0].lower()])
+            return
+        
     def dungeon_walk(self):
-        print "Welcome to", self.dungeon.title, "!"
-        print self.location.display_room()
+        write("Welcome to" +  self.dungeon.title + "!")
+        self.location.display_room()
         while 1==1:
-            c = raw_input("> ")
-            if c.__len__() != 0:                
+            c = input("> ")
+            if c.__len__() != 0: 
+                self.parse_action(c)
+               
                 if c.lower()[0] == 'q':
-                    print "Goodbye!"
+                    write("Goodbye!")
                     break
-                if DIR_CHOICES.__contains__(c.lower()):
-                    self.move(c)
-                
-                
 
-
-
+    def show_inventory(self):
+        if not self.inventory:
+            pass
+ 
+class Item:
+    title = None
+    description = None
+    
+    def __init__(self, title='', description=''):
+        self.title = title
+        self.description = description
+    
+    def display(self):
+        write(self.title + ' - ' + self.description)
+    
+    def edit(self, title='', description='', location=None):
+        if not title:
+            t = input("New title (return to keep the same): ")
+            self.title = t
+        if not description:
+            d = input("New description (return to keep the same):")
+            self.description = d
+        
+            
+        
